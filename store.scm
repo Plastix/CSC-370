67992c9edd64106d1261e422d14196a79f991aaa
Plastix <aidanpieper@gmail.com>
2017-03-05 09:09:49 -0500

    Fix vector-based store implementation

--- store.scm
+++ store.scm
@@ -13,7 +13,10 @@
 		(next number?))
 )
 
-;; Reference to next free cell in store
+;; Index of next free cell in store
+;; The object at said index will contain a reference to the next
+;; free cell's index
+;;
 ;; -1 means no free cell
 (define free-list! -1)
 
@@ -35,6 +38,9 @@
      [(> l u) '()]
      [else (cons l (range (+ l 1) u))])))
 
+(define no-free?
+	(lambda () (= free-list! -1)))
+
 ;; (newref! val) takes a value val adds to the the-store!, and returns
 ;; a ref-val to the added value val.
 (define newref!
@@ -43,13 +49,15 @@
 
 			;; No free slots in store
 			;; We must grow store!
-			[(= free-list! -1) 
-
+			[(no-free?)
 				(let*
-					([numfull (vector-length the-store!)]
-					[newlen (+ 1 (* 2 numfull))]
-					[newstore (make-vector newlen (free-cell (- newlen 1) -1))]
-					[i 0])
+					(
+					[numfull (vector-length the-store!)] ; Number of items currently in our store
+					[newlen (+ 1 (* 2 numfull))] ; New length of the store
+					[lastIndex (- newlen 1)] ; Index of the last item in the new store
+					[newstore (make-vector newlen (free-cell lastIndex -1))] ; New store vector
+					[i 0]
+					)
 
 					; Copy old values to new store
 					(vector-for-each 
@@ -59,7 +67,7 @@
 						) 
 						the-store!)
 
-					; Insert new val
+					; Insert new val at numfull position
 					(vector-set! newstore numfull (expval-cell val))
 
 					; Update new frees cells
@@ -69,44 +77,40 @@
 							(vector-set! newstore i (free-cell i (+ i 1)))
 						)
 						#f
-						(range (+ numfull 1) (- newlen 2)))
-
-					;(display "newlen ")
-					;(display newlen)
-					;(display "\n")
-					;(display "numfull ")
-					;(display numfull)
-					;(display "\n")
+						(range (+ numfull 1) (- lastIndex 1)))
 
 					; Update free list
-					(set! free-list
-							(cond 
-								[(= newlen 1) -1]
-								[else (+ numfull 2)])) ; ????							    
+					(set! free-list!
+							(if (>= (+ numfull 1) newlen)
+								-1
+								(+ numfull 1)))						    
 
 					; Update store
 					(set! the-store! newstore)
 			
+					; Return reference to newly inserted item
 					(ref-val numfull)
 				)]
 				
 			;; We have a free cell, stick val there!
 			[else
+				; First look up index of free-list to get free-cell
 				(let ([c (vector-ref the-store! free-list!)])
 					(cases cell c
 						[free-cell (ref next)
-							(set! free-list! next)
+				
+							; Set val to current index of free cell
 							(vector-set! the-store! ref (expval-cell val))
+
+							; Update free list to next index pointer
+							(set! free-list! next)
+
+							; Return reference to newly inserted item
 							(ref-val ref)]
+						[else (raise-exception 'newref "Something went wrong! Couldn't find room for a new reference!")]
 					))]
 
-		))
-
-
-
-		;(set! the-store! (append the-store! (list val)))
-		;(ref-val (- (length the-store!) 1))
-)
+		)))
 
 ;; (deref ev) expects that ev is a reference (ref-val ref), and
 ;; returns the value of associated with ref in the store.
